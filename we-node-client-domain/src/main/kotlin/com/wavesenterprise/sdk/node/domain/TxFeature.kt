671defdf0cc7cb104f1e7350f60149c24cb31d93
dgeorgiev <dgeorgiev@wavesenterprise.com>
2023-06-15 18:46:36 +0300

    [WTCH-158] Add TxVersionDictionary

---
+++ we-node-client-domain/src/main/kotlin/com/wavesenterprise/sdk/node/domain/TxFeature.kt
@@ -0,0 +1,35 @@
+package com.wavesplatform.vst.node.dto.tx
+
+import com.wavesenterprise.sdk.node.domain.TxType
+
+enum class TxFeature {
+    SPONSORED_FEES,
+    SMART_ACCOUNTS,
+    SMART_ASSETS,
+    DATA_TRANSACTION,
+    GRPC_CONTRACTS,
+    MASS_TRANSFER,
+    ATOMIC;
+
+    val bitmask by lazy { 1 shl ordinal }
+}
+
+class DictionaryTxVersion private constructor(
+    val type: TxType,
+    val version: Int,
+    private val features: Int
+) {
+
+    constructor(
+        type: TxType,
+        version: Int,
+        features: Set<TxFeature> = setOf()
+    ) : this(
+        type = type,
+        version = version,
+        features = features.fold(0) { acc, feature -> acc or feature.bitmask }
+    )
+
+    fun supports(features: Set<TxFeature>) =
+        this.features == features.fold(this.features) { acc, feature -> acc or feature.bitmask }
+}
