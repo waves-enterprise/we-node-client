4519466dbf27176858602287ab22ec60c9ec83f3
dgeorgiev <dgeorgiev@web3tech.ru>
2023-09-18 16:24:12 +0300

    Edit bom publishing config

--- we-node-client-bom/build.gradle.kts
+++ we-node-client-bom/build.gradle.kts
@@ -1,3 +1,6 @@
+val gitHubProject = "waves-enterprise/we-node-client"
+val githubUrl = "https://github.com/$gitHubProject"
+
 plugins {
     `maven-publish`
     `java-platform`
@@ -19,6 +22,34 @@ publishing {
     publications {
         create<MavenPublication>("we-node-client-bom") {
             from(components["javaPlatform"])
+
+            pom {
+                packaging = "pom"
+                name.set(project.name)
+                url.set(githubUrl)
+                description.set("WE Node Client BOM")
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:$githubUrl")
+                    developerConnection.set("scm:git@github.com:$gitHubProject.git")
+                    url.set(githubUrl)
+                }
+
+                developers {
+                    developer {
+                        id.set("kt3")
+                        name.set("Stepan Kashintsev")
+                        email.set("kpote3@gmail.com")
+                    }
+                }
+            }
         }
     }
 }
