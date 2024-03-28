a8457ca10dd16532be5be9b5411d2995bc2ad500
dgeorgiev <dgeorgiev@web3tech.ru>
2024-03-28 14:15:43 +0300

    Remove crypto pro implementation

--- we-tx-signer/we-tx-signer-bouncy-castle/build.gradle.kts
+++ we-tx-signer/we-tx-signer-bouncy-castle/build.gradle.kts
@@ -1,12 +1,13 @@
-val bouncycastleVersion: String by project
-
 dependencies {
     implementation(kotlin("stdlib"))
 
+    api(project(":we-node-client-domain"))
     api(project(":we-node-client-blocking-client"))
     api(project(":we-tx-signer:we-tx-signer-api"))
-    api(project(":we-node-client-http:we-node-client-feign-client"))
-    implementation(project(":we-node-client-json"))
+    api(project(":we-tx-signer:we-tx-signer-code"))
+
+    implementation("org.bouncycastle:bcprov-jdk15on")
+    implementation("org.bouncycastle:bcpkix-jdk15on")
 
     testImplementation("io.mockk:mockk")
     testImplementation("org.junit.jupiter:junit-jupiter-api")
