7ff3246acb05a36bebb647611ea87b93848b6c8e
dgeorgiev <dgeorgiev@web3tech.ru>
2024-05-28 12:18:00 +0300

    [WTCH-255] Implement GenesisPermitTransaction grpc mapper (proto -> domain)

--- we-node-client-grpc/we-node-client-grpc-mapper/src/main/kotlin/com/wavesenterprise/sdk/node/client/grpc/mapper/tx/GenesisPermitTxMapper.kt
+++ we-node-client-grpc/we-node-client-grpc-mapper/src/main/kotlin/com/wavesenterprise/sdk/node/client/grpc/mapper/tx/GenesisPermitTxMapper.kt
@@ -1,10 +1,35 @@
 package com.wavesenterprise.sdk.node.client.grpc.mapper.tx
 
+import com.wavesenterprise.sdk.node.client.grpc.mapper.GrpcTypesMapper.byteArray
+import com.wavesenterprise.sdk.node.client.grpc.mapper.RoleMapper.domain
+import com.wavesenterprise.sdk.node.domain.Address
+import com.wavesenterprise.sdk.node.domain.Fee
+import com.wavesenterprise.sdk.node.domain.Signature
+import com.wavesenterprise.sdk.node.domain.Timestamp
+import com.wavesenterprise.sdk.node.domain.TxId
+import com.wavesenterprise.sdk.node.domain.TxVersion
 import com.wavesenterprise.sdk.node.domain.tx.GenesisPermitTx
 import com.wavesenterprise.transaction.protobuf.GenesisPermitTransactionOuterClass.GenesisPermitTransaction
 
 object GenesisPermitTxMapper {
+
     fun dtoInternal(tx: GenesisPermitTx): GenesisPermitTransaction {
         TODO("Not yet implemented")
     }
+
+    @JvmStatic
+    fun GenesisPermitTransaction.domain(version: TxVersion): GenesisPermitTx =
+        domainInternal(this, version)
+
+    @JvmStatic
+    internal fun domainInternal(tx: GenesisPermitTransaction, version: TxVersion): GenesisPermitTx =
+        GenesisPermitTx(
+            id = TxId(tx.id.byteArray()),
+            target = Address(tx.target.byteArray()),
+            role = tx.role.domain(),
+            fee = Fee(tx.fee),
+            timestamp = Timestamp.fromUtcTimestamp(tx.timestamp),
+            version = version,
+            signature = Signature(tx.signature.byteArray()),
+        )
 }
--- we-node-client-grpc/we-node-client-grpc-mapper/src/test/kotlin/com/wavesenterprise/sdk/node/client/grpc/mapper/tx/GenesisPermitTxMapperTest.kt
+++ we-node-client-grpc/we-node-client-grpc-mapper/src/test/kotlin/com/wavesenterprise/sdk/node/client/grpc/mapper/tx/GenesisPermitTxMapperTest.kt
@@ -1,49 +1,50 @@
 package com.wavesenterprise.sdk.node.client.grpc.mapper.tx
 
 import com.wavesenterprise.sdk.node.client.grpc.mapper.AddressMapper.byteString
+import com.wavesenterprise.sdk.node.client.grpc.mapper.RoleMapper.dto
 import com.wavesenterprise.sdk.node.client.grpc.mapper.SignatureMapper.byteString
 import com.wavesenterprise.sdk.node.client.grpc.mapper.TxIdMapper.byteString
-import com.wavesenterprise.sdk.node.client.grpc.mapper.tx.GenesisTxMapper.domain
+import com.wavesenterprise.sdk.node.client.grpc.mapper.tx.GenesisPermitTxMapper.domain
 import com.wavesenterprise.sdk.node.domain.Address
-import com.wavesenterprise.sdk.node.domain.Amount
 import com.wavesenterprise.sdk.node.domain.Fee
+import com.wavesenterprise.sdk.node.domain.Role
 import com.wavesenterprise.sdk.node.domain.Signature
 import com.wavesenterprise.sdk.node.domain.Timestamp
 import com.wavesenterprise.sdk.node.domain.TxId
 import com.wavesenterprise.sdk.node.domain.TxVersion
-import com.wavesenterprise.transaction.protobuf.genesisTransaction
+import com.wavesenterprise.transaction.protobuf.genesisPermitTransaction
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 
-class GenesisTxMapperTest {
+class GenesisPermitTxMapperTest {
 
     @Test
-    fun `should map to GenesisTx (all fields)`() {
+    fun `should map to GenesisPermitTx (all fields)`() {
         val txId = TxId.fromBase58("DnK5Xfi2wXUJx9BjK9X6ZpFdTLdq2GtWH9pWrcxcmrhB")
-        val txAmount = Amount(1000L)
+        val txTarget = Address.fromBase58("3N9vL3apA4j2L5PojHW8TYmfHx9Lo2ZaKPB")
+        val txRole = Role.SENDER
         val txFee = Fee(10L)
         val txTimestamp = Timestamp(1716881331027L)
-        val txRecipient = Address("3N9vL3apA4j2L5PojHW8TYmfHx9Lo2ZaKPB".toByteArray())
         val txSignature = Signature(
             "2Gns72hraH5yay3eiWeyHQEA1wTqiiAztaLjHinEYX91FEv62HFW38Hq89GnsEJFHUvo9KHYtBBrb8hgTA9wN7DM".toByteArray()
         )
         val txVersion = TxVersion.fromInt(1)
 
-        val grpcTx = genesisTransaction {
+        val grpcTx = genesisPermitTransaction {
             id = txId.byteString()
-            amount = txAmount.value
+            target = txTarget.byteString()
+            role = txRole.dto()
             fee = txFee.value
             timestamp = txTimestamp.utcTimestampMillis
-            recipient = txRecipient.byteString()
             signature = txSignature.byteString()
         }
 
         grpcTx.domain(txVersion).apply {
             id.bytes shouldBe grpcTx.id.toByteArray()
-            amount.value shouldBe grpcTx.amount
+            target.bytes shouldBe grpcTx.target.toByteArray()
+            role.code shouldBe grpcTx.role.id
             fee.value shouldBe grpcTx.fee
             timestamp.utcTimestampMillis shouldBe grpcTx.timestamp
-            recipient.bytes shouldBe grpcTx.recipient.toByteArray()
             signature.bytes shouldBe grpcTx.signature.toByteArray()
             version.value shouldBe txVersion.value
         }
