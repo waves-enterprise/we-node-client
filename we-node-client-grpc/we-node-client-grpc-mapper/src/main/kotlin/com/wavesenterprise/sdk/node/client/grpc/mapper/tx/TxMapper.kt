434dfe1e144a9e066eaaab3aed0849ee5c1097cd
dgeorgiev <dgeorgiev@web3tech.ru>
2024-05-29 13:58:01 +0000

    [WTCH-264] Implement SponsorFeeTransaction grpc mapper (proto -> domain)

--- we-node-client-grpc/we-node-client-grpc-mapper/src/main/kotlin/com/wavesenterprise/sdk/node/client/grpc/mapper/tx/TxMapper.kt
+++ we-node-client-grpc/we-node-client-grpc-mapper/src/main/kotlin/com/wavesenterprise/sdk/node/client/grpc/mapper/tx/TxMapper.kt
@@ -18,6 +18,7 @@ import com.wavesenterprise.sdk.node.client.grpc.mapper.tx.PermitTxMapper.domain
 import com.wavesenterprise.sdk.node.client.grpc.mapper.tx.PolicyDataHashTxMapper.domain
 import com.wavesenterprise.sdk.node.client.grpc.mapper.tx.RegisterNodeTxMapper.domain
 import com.wavesenterprise.sdk.node.client.grpc.mapper.tx.ReissueTxMapper.domain
+import com.wavesenterprise.sdk.node.client.grpc.mapper.tx.SponsorFeeTxMapper.domain
 import com.wavesenterprise.sdk.node.client.grpc.mapper.tx.TransferTxMapper.domain
 import com.wavesenterprise.sdk.node.client.grpc.mapper.tx.TxMapper.domain
 import com.wavesenterprise.sdk.node.client.grpc.mapper.tx.UpdateContractTxMapper.domain
@@ -117,7 +118,7 @@ object TxMapper {
             Transaction.TransactionCase.LEASE_CANCEL_TRANSACTION,
             -> leaseCancelTransaction.domain(version)
             Transaction.TransactionCase.SPONSOR_FEE_TRANSACTION,
-            -> TODO("Not yet implemented")
+            -> sponsorFeeTransaction.domain(version)
             Transaction.TransactionCase.SET_ASSET_SCRIPT_TRANSACTION,
             -> TODO("Not yet implemented")
             Transaction.TransactionCase.DATA_TRANSACTION,
