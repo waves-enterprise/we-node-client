e796205be463f2c7875036ab4df9390b46b81bd2
dgridnev <dgridnev@web3tech.ru>
2024-05-30 12:52:52 +0300

    [WTCH-273] Implement CreateContractTransaction grpc mapper (proto -> domain)

--- we-node-client-grpc/we-node-client-grpc-mapper/src/test/kotlin/com/wavesenterprise/sdk/node/client/grpc/mapper/tx/TxMapperTestUtil.kt
+++ we-node-client-grpc/we-node-client-grpc-mapper/src/test/kotlin/com/wavesenterprise/sdk/node/client/grpc/mapper/tx/TxMapperTestUtil.kt
@@ -4,3 +4,10 @@ import com.google.protobuf.ByteString
 
 internal fun byteString(value: String): ByteString =
     ByteString.copyFromUtf8(value)
+
+internal fun <T, U> cartesianProduct(i1: Iterable<T>, i2: Iterable<U>): List<Pair<T, U>> =
+    i1.flatMap { first ->
+        i2.map { second ->
+            first to second
+        }
+    }
