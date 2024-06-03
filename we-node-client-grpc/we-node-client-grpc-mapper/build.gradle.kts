6c61a55c9a8fd1cf99bc9fff50401e00bc7e6f23
dgeorgiev <dgeorgiev@web3tech.ru>
2024-06-03 09:12:13 +0000

    [WTCH-279] Implement AtomicTransaction grpc mapper (proto -> domain)

--- we-node-client-grpc/we-node-client-grpc-mapper/build.gradle.kts
+++ we-node-client-grpc/we-node-client-grpc-mapper/build.gradle.kts
@@ -3,6 +3,7 @@ dependencies {
 
     api(project(":we-node-client-grpc:we-node-client-grpc-java"))
 
+    testImplementation("io.mockk:mockk")
     testImplementation("org.junit.jupiter:junit-jupiter-api")
     testImplementation("org.junit.jupiter:junit-jupiter-params")
     testImplementation("org.junit.jupiter:junit-jupiter-engine")
